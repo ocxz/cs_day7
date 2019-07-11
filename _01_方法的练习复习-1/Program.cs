ea52afb21fc01d52d7abd8e23dbc4ed32b528a0d
ocxz <2252779530@qq.com>
2019-07-12 03:22:45 +0800

    添加项目文件。

---
+++ _01_方法的练习复习-1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01_方法的练习复习_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
